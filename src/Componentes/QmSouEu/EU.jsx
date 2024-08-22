008c7bd228c92f94a00151cab9362354e95d3c3c
Peterson Couto <142356426+petersonfernandocouto@users.noreply.github.com>
2024-08-21 21:14:41 -0300

    Atualização 2.1
    ícones de redes sociais

--- src/Componentes/QmSouEu/EU.jsx
+++ src/Componentes/QmSouEu/EU.jsx
@@ -1,5 +1,10 @@
 import './index.scss';
 import EuP from '../../../public/IMG/EuP.jpg'
+import { BsWhatsapp } from "react-icons/bs";
+import { CiLinkedin } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 
 function EU() {
 
@@ -31,6 +36,36 @@ function EU() {
                                 <br /><br />
 
                                 <p>Meu nome é Peterson Fernando Couto, estudante de Análise e Desenvolvimento de Sistemas na Universidade Paulista e também de <br />Front-end na escola DNC. Como desenvolvedor, realizo projetos freelancer fornecidos pela DNC, que envolvem reuniões, análises e desenvolvimento técnico. Estou comprometido em aplicar minhas habilidades e conhecimentos para entregar soluções eficazes e inovadoras.</p>
+
+                            </div>
+
+                            <div className='Contatos'>
+                                    
+                                <a href='https://WA.me/5519991955915' target='_blank' rel='noopener noreferrer'>
+
+                                    <BsWhatsapp />
+
+                                </a>
+
+                                <a href='https://www.linkedin.com/in/peterson-fernando-6b3709133/' target='_blank' rel='noopener noreferrer'>
+
+                                   <CiLinkedin />
+
+                                </a>
+
+                                <a href='https://www.instagram.com/petersonfernando17/' target='_blank' rel='noopener noreferrer'>
+
+                                    <FaInstagram />
+
+                                </a>
+
+
+                                <a href='https://github.com/petersonfernandocouto' target='_blank' rel='noopener noreferrer'>
+
+                                    <FaGithub />                                 
+
+                                </a>
+
 
                             </div>
                         
